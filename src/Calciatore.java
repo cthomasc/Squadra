9acaf4dd9ecd656012b1564572cc0044deef018c
thom.iobstraibizer <trentinphoto68@gmail.com>
2023-10-16 08:51:38 +0200

    feat: costruttore Calciatore

--- src/Calciatore.java
+++ src/Calciatore.java
@@ -1,4 +1,15 @@
 public class Calciatore extends Persona {
+    private String ruolo;
+    private int nMaglia;
+    private int nGoal;
+
+    public Calciatore (String ruolo, int nMaglia, int nGoal, //Attributi classe Calciatore
+                       String nome, int annoNascita, String sesso){ //Attributi classe Persona.
+        super(nome,annoNascita,sesso);
+        this.ruolo = ruolo;
+        this.nMaglia = nMaglia;
+        this.nGoal = nGoal;
+    }
 
 
 }
