984faadf861410e55c46e2411db49db8d9d7526a
thom.iobstraibizer <trentinphoto68@gmail.com>
2023-10-16 08:42:00 +0200

    test: getter, setter e clone

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,10 @@ public class Main {
     public static void main(String[] args) {
         Persona p = new Persona("Thomas",2006,"Maschio");
         System.out.println(p.toString());
-
+        p.getNome();
+        p.getAnnoNascita();
+        p.getSesso();
+        p.setSesso("Trans");
+        p.clone();
     }
 }
