799917f9797169093ef1836b6d8737e6e4ac9fa3
thom.iobstraibizer <trentinphoto68@gmail.com>
2023-10-16 08:24:00 +0200

    feat: metodi base

--- src/Persona.java
+++ src/Persona.java
@@ -1,3 +1,19 @@
 public class Persona {
+    private String nome;
+    private int annoNascita;
+    private String sesso;
+
+    public Persona (String nome, int annoNascita, String sesso){
+        this.nome = nome;
+        this.annoNascita = annoNascita;
+        this.sesso = sesso;
+    }
+    public String toString(){
+        String rit = "";
+        rit += "Nome: " + this.nome;
+        rit+= "\nAnno di nascita: " + this.annoNascita;
+        rit += "\nGenere: " + this.sesso;
+        return rit;
+    }
 
 }
